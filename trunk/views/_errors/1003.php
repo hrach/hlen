65896c663adefc6b195a657df0b648a8b210b9cc
skrasek.jan@gmail.com <skrasek.jan@gmail.com@25f1c60d-333a-0410-bdaa-d3da786970b0>
2007-11-25 12:44:28 +0000

    Odstraněný soubor/složka

--- trunk/views/_errors/1003.php
+++
@@ -1,8 +0,0 @@
-<div id="err_message">
-
-<h1>CHYBA: Nenalezen soubor view</h1>
-<p>Zkontrolujte, zda-li jste vytvořili soubor view: <em><?= $arg[1] ?></em></p>
-
-<p>Po vypnutí ladícího režimu se při chybě zobrazí klasická E404.</p>
-
-</div>
