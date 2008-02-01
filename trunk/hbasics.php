fb0025f3410c1e44726a810044a75a88825a0bf0
skrasek.jan@gmail.com <skrasek.jan@gmail.com@25f1c60d-333a-0410-bdaa-d3da786970b0>
2008-02-01 12:02:05 +0000

    HRouter - renamed vars: $defaultController $defaultAction
    new created
    $defaultRuleKey
    $rules
    
    also fixes and code optimalization.

--- trunk/hbasics.php
+++ trunk/hbasics.php
@@ -4,7 +4,7 @@
  * HLEN FRAMEWORK
  *
  * @author     Jan Skrasek <skrasek.jan@gmail.com>
- * @copyright  Copyright (c) 2007, Jan Skrasek
+ * @copyright  Copyright (c) 2008, Jan Skrasek
  * @package    Hlen
  */
 
@@ -16,9 +16,9 @@ class HBasics
     {
         if (file_exists($fileName)) {
             if ($once) {
-                require_once($fileName);
+                require_once $fileName;
             } else {
-                require($fileName);
+                require $fileName;
             }
             return true;
         }
@@ -38,21 +38,21 @@ class HBasics
     }
 
     public static function coolUrl($title, $alternative = false) {
+        $title = preg_replace('~[^\\pL0-9_]+~u', '-', $title);
+        $title = trim($title, "-");
         if ($alternative) {
             // author David GRUDL
             // site http://www.dgx.cz/
             static $tbl = array("\xc3\xa1"=>"a","\xc3\xa4"=>"a","\xc4\x8d"=>"c","\xc4\x8f"=>"d","\xc3\xa9"=>"e","\xc4\x9b"=>"e","\xc3\xad"=>"i","\xc4\xbe"=>"l","\xc4\xba"=>"l","\xc5\x88"=>"n","\xc3\xb3"=>"o","\xc3\xb6"=>"o","\xc5\x91"=>"o","\xc3\xb4"=>"o","\xc5\x99"=>"r","\xc5\x95"=>"r","\xc5\xa1"=>"s","\xc5\xa5"=>"t","\xc3\xba"=>"u","\xc5\xaf"=>"u","\xc3\xbc"=>"u","\xc5\xb1"=>"u","\xc3\xbd"=>"y","\xc5\xbe"=>"z","\xc3\x81"=>"A","\xc3\x84"=>"A","\xc4\x8c"=>"C","\xc4\x8e"=>"D","\xc3\x89"=>"E","\xc4\x9a"=>"E","\xc3\x8d"=>"I","\xc4\xbd"=>"L","\xc4\xb9"=>"L","\xc5\x87"=>"N","\xc3\x93"=>"O","\xc3\x96"=>"O","\xc5\x90"=>"O","\xc3\x94"=>"O","\xc5\x98"=>"R","\xc5\x94"=>"R","\xc5\xa0"=>"S","\xc5\xa4"=>"T","\xc3\x9a"=>"U","\xc5\xae"=>"U","\xc3\x9c"=>"U","\xc5\xb0"=>"U","\xc3\x9d"=>"Y","\xc5\xbd"=>"Z");
-            return strtr($title, $tbl);
+            $title = strtr($title, $tbl);
         } else {
             // author Jakub VRANA
             // site http://php.vrana.cz
-            $title = preg_replace('~[^\\pL0-9_]+~u', '-', $title);
-            $title = trim($title, "-");
             $title = iconv("utf-8", "us-ascii//TRANSLIT", $title);
-            $title = strtolower($title);
-            $title = preg_replace('~[^-a-z0-9_]+~', '', $title);
-            return $url;
         }
+        $title = strtolower($title);
+        $title = preg_replace('~[^-a-z0-9_]+~', '', $title);
+        return $url;
     }
 
     public static function getVal()
--- trunk/hrouter.php
+++ trunk/hrouter.php
@@ -14,61 +14,56 @@ require_once dirname(__FILE__) . '/hhttp.php';
 class HRouter
 {
 
-    public static $routing = false;
-    public static $url;
     public static $segment = array();
-    public static $prefix = array();
+    public static $routing = false;
 
-    public static $base = ':controller/:action';
-    public static $rule = array();
     public static $defaultController = '';
     public static $defaultAction = 'index';
-    
+    public static $defaultRuleKey = 0;
+
+    public static $rules = array(':controller/:action/*');
+
+    public static $namedArguments = array();
+    public static $namedArgumentsSeparator = ':';
+
     public static $controller;
     public static $action;
     public static $args = array();
-    public static $multiArgs = true;
+    public static $rule = array();
 
-    public static $naSeparator = ':';
-    public static $system = false;
     public static $service;
+    public static $system = false;
 
     private static $services = array();
 
 
-    public static function start($url, $router)
+    public static function start($url, $router, $isFile = true)
     {
         self::$segment = HHttp::urlToArray($url);
-       
-        if (is_callable($router)) {
+
+        if (!$isFile) {
             call_user_func($router);
         } else {
-            if (file_exists($router)) {
-                include $router;
-            }
+            include $router;
         }
 
         if (!self::$routing) {
-            self::connect(self::$base, array(
-                'multiArgs' => true
-            ));
-            
-            if (self::$base == ':controller/:action') {
-                self::connect(':controller');
-                self::$rule = array(':controller', ':action');
-            }
-        }
+            self::connect(self::$rules[0]);
 
-        if (!self::$routing) {
-            self::$controller = self::$defaultController;
-            self::$action = self::$defaultAction;
-            self::$rule = HHttp::urlToArray(self::$base);
+            if (self::$rules[0] == ':controller/:action/*') {
+                self::connect(':controller', array('ruleKey' => 0));
+            }
         }
     }
 
-    public static function addService($services)
+    public static function addService($service)
     {
-        self::$services = array_merge(self::$services, (array) $services);
+        self::$services[] = $service;
+
+        if (self::$segment[count(self::$segment)-1] === $service) {
+            self::$service = $service;
+            array_pop(self::$segment);
+        }
     }
 
     public static function rewrite($rule, $newUrl)
@@ -84,79 +79,89 @@ class HRouter
         return false;
     }
 
-    public static function connect($rule, $options = array())
+    public static function connect($rule, array $options = array(), array $restrictions = array())
     {
+        static $ruleModificators = array(':controller', ':action', ':arg');
+
         if (self::$routing) {
             return false;
         }
-        
-        self::removeServicSegment();
-        
-        if (!isset($options['multiArgs'])) {
-            $options['multiArgs'] = false;
+
+        $router['controller']  = self::$defaultController;
+        $router['action']      = self::$defaultAction;
+        $router['args']        = array();
+
+        $rule          = HHttp::urlToArray($rule);
+        $segmentCount  = count(self::$segment);
+
+        if ($rule[count($rule) - 1] === '*') {
+            array_pop($rule);
+            $multiArgs = true;
+        } else {
+            $multiArgs = false;
         }
 
-        $router['controller'] = self::$defaultController;
-        $router['action'] = self::$defaultAction;
-        $router['args'] = array();
-        
-        $rule = HHttp::urlToArray($rule);
-        $key = -1;
-        
-        if ((count($rule) === 0 && (count($rule) < count(self::$segment) && $options['multiArgs'] === false))
-         || (count($rule) > count(self::$segment))) {
+        $ruleCount = count($rule);
+
+        if (($ruleCount === 0 && $multiArgs === true) || ($ruleCount < $segmentCount && $multiArgs === false) || $ruleCount > $segmentCount) {
             return false;
         }
-        
+
+        $key = -1;
         foreach ($rule as $key => $val) {
-            if (in_array($val, array(':controller', ':action', ':arg'))) {
-                if ($val === ':arg') {
-                    $nArg = self::removePrefix(self::getSegment($key));
-                    $router['args'][$nArg[0]] = $nArg[1];
+            if ($val === ':arg') {
+                $newArg = self::removePrefix(self::getSegment($key));
+                if (is_integer($newArg[0])) {
+                    $router['args'][] = $nArg[1];
                 } else {
-                    $router[substr($val, 1)] = self::getSegment($key);
+                    $router['args'][$nArg[0]] = $nArg[1];
                 }
+            } elseif ($val === ':controller') {
+                $router['controller'] = self::getSegment($key);
+            } elseif ($val === ':action') {
+                $router['action'] = self::getSegment($key);
             } elseif ($val !== self::getSegment($key)) {
                 return false;
             }
         }
 
-        foreach ($options as $att => $option) {
-            if (in_array($att, array('controller', 'action'))) {
-                $router[$att] = $option;
-            }
+        if (isset($options['controller'])) {
+            $router['controller'] = $options['controller'];
         }
-        
-        if ($options['multiArgs']) {
+
+        if (isset($options['action'])) {
+            $router['action'] = $options['action'];
+        }
+
+        if (isset($options['ruleKey']) && isset(self::$rules[$options['ruleKey']])) {
+            $router['rule'] = HHttp::urlToArray(self::$rules[$options['ruleKey']]);
+        } else {
+            $router['rule'] = $rule;
+        }
+
+        if ($multiArgs) {
             while (self::getSegment(++$key) !== false) {
                 $nArg = self::removePrefix(self::getSegment($key));
                 $router['args'][$nArg[0]] = $nArg[1];
             }
         }
-        
+
         if (isset($options['args'])) {
             foreach ((array) $options['args'] as $argName => $arg) {
                 if (!is_integer($argName)) {
-                    $newArg = $argName . HRouter::$naSeparator . $arg;
-                    $newArg = self::removePrefix($newArg);
-                    $router['args'][$argName] = $newArg[1];
+                    $router['args'][$argName] = $arg;
                 } else {
                     $router['args'][] = $arg;
                 }
             }
         }
 
-        if (isset($options['rule'])) {
-            $rule = HHttp::urlToArray($options['rule']);
-        }
-
         self::$controller = $router['controller'];
         self::$action     = $router['action'];
         self::$args       = $router['args'];
-        
+        self::$rule       = $router['rule'];
+
         self::$routing    = true;
-        self::$rule       = $rule;
-        self::$multiArgs  = $options['multiArgs'];
         return true;
     }
 
@@ -165,39 +170,18 @@ class HRouter
         if (isset(self::$segment[$x])) {
             return self::$segment[$x];
         }
-
         return false;
     }
 
     private static function removePrefix($arg)
     {
-        static $index = 0;
-
-        foreach (self::$prefix as $prefix) {
+        foreach (self::$namedArguments as $prefix) {
             $part = substr($arg, 0, strlen($prefix) + 1);
-            if ($prefix . self::$naSeparator === $part) {
+            if ($prefix . self::$namedArgumentsSeparator === $part) {
                 return array($prefix, substr($arg, strlen($prefix) + 1));
             }
         }
-
-        return array($index++, $arg);
-    }
-
-    private static function removeServicSegment()
-    {
-        if (empty(self::$services)) {
-            return false;
-        }
-
-        foreach (self::$services as $service) {
-            if (self::$segment[count(self::$segment)-1] === $service) {
-                self::$service = $service;
-                array_pop(self::$segment);
-                return true;
-            }
-        }
-
-        return false;
+        return array(0, $arg);
     }
 
 }
