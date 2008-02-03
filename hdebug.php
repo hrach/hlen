948be0fe9b4353e40585eb455b77fddf67c3be93
skrasek.jan@gmail.com <skrasek.jan@gmail.com@25f1c60d-333a-0410-bdaa-d3da786970b0>
2008-02-03 13:19:55 +0000

    * rename class HLoader to HAutoLoad * new method for getting classes in HAutoLoad * some fixes in HApplication - now you define all config in index.php - you nwe bootstrap;

--- hdebug.php
+++ hdebug.php
@@ -26,12 +26,9 @@ class HDebug
 
     public function mark($var)
     {
-        if ((class_exists('HApplication', false) && HConfigure::read('Core.debug') > 1)
-         || !class_exists('HApplication', false)) {
-            echo '<span style="color: black;background: white;" class="debigging-marks">';
-            echo 'Debug mark: <strong style="color: red;">' . $var . '</strong>';
-            echo '</span><br/>';
-        }
+        echo '<span style="color: black;background: white;" class="debigging-marks">';
+        echo 'Debug mark: <strong style="color: red;">' . $var . '</strong>';
+        echo '</span><br/>';
     }
 
 }
